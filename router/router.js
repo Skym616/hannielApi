95d29fbcae33aaa421470530b3e00da3cfbbcecf
Skym616 <yannicksimo616@gmail.com>
2022-05-12 07:32:16 +0100

    nothing

--- router/router.js
+++ router/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controller/controller');
 
+router.get('/', ctrl.msg);
 router.post('/users',ctrl.postUser);
 router.get('/users',ctrl.getUsers);
 
