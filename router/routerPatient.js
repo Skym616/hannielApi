1fab90a7d695df8a6c6a69895fa45f5102adf28b
Skym616 <yannicksimo616@gmail.com>
2022-07-13 17:22:32 +0100

    user patient

--- router/routerPatient.js
+++ router/routerPatient.js
@@ -7,6 +7,7 @@ const auth = require('../middleware/auth');
 router.route('/').get(ctrlPatient.msg);
 router.route('/signIn').post(ctrlPatient.signIn);
 router.route('/signUp').post(ctrlPatient.signUp);
+router.route('/update').put(auth, multer, ctrlPatient.update);
 router.route('/post').post(auth, multer, ctrlPatient.post);
 router.route('/all/pharmacy').get(auth, ctrlPatient.getAllPharmacy);
 router.route('/g/:idPatient').get(auth, ctrlPatient.getOnePatient);
