7a984d443643e4a716b5647ae19b6f67dd967cc4
Skym616 <yannicksimo616@gmail.com>
2022-07-14 12:40:18 +0100

    admin Controller add

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./hanniel-app-firebase-adminsdk-7h1xd-ded869e69b.json');
+const serviceAccount = require('./teemesapp-firebase-adminsdk-ypeja-9b13b9aa97.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
